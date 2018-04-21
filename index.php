98a22400f7952b6e51ec0d16d4159d1fcc516505
Davi-Notebook <davidmaccurso@gmail.com>
2018-04-20 22:25:23 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Agenda en PHP</title>
+    <link rel="stylesheet" href="css/Main.css" media="screen">
+  </head>
+  <body>
+    <div class="contenedor">
+      <h1>Agenda de Contactos</h1>
+
+      <div class="contenido">
+        <h2>Nuevo Contactos</h2>
+        <form class="" action="crear.php" method="post">
+            <div class="campo">
+              <label for="nombre">Nombre
+                <input type="text" name="nombre" placeholder="Nombre">
+              </label>
+            </div>
+            <div class="campo">
+              <label for="numero">Numero
+                <input type="text" name="numero" placeholder="Numero">
+              </label>
+            </div>
+        </form>
+      </div>
+
+
+    </div>
+  </body>
+</html>
